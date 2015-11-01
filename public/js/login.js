e3b09969fea37e1288d8e169134ba64f6a416806
Timothy Higinbottom <timhigins@gmail.com>
2015-11-01 07:29:52 -0500

    stuff

--- public/js/login.js
+++ public/js/login.js
@@ -4,39 +4,46 @@ root = new Firebase('https://testtaker.firebaseio.com/');
 // TODO: fix onclick event listener
 
 $('#studentLogin').on('click', function() {
-    console.log('hi');
-    var students = root.child('students');
-    students.authWithPassword({
-        "email": $('#user').val(),
-        "password": $('#pass').val()
-    }, function(error, authData) {
-        if (error) {
-            // TODO: error
-            console.log('error signing in');
-        } else {
-            // TODO: success
-            console.log(authData);
-            $.post('/login', {'uid': authData.uid, 'type': 'student', authData: authData}, function(data){
-                $('html').html(data);
-            });
-        }
-    });
+  console.log('hi');
+  var students = root.child('students');
+  students.authWithPassword({
+    "email": $('#user').val(),
+    "password": $('#pass').val()
+  }, function(error, authData) {
+    if (error) {
+      // TODO: error
+      console.log('error signing in');
+    } else {
+      // TODO: success
+      console.log(authData);
+      $.post('/login', {
+        'uid': authData.uid,
+        'type': 'student',
+        authData: authData
+      }, function(data) {
+        $('html').html(data);
+      });
+    }
+  });
 });
 
 $('#teacherLogin').on('click', function() {
-    console.log('hi');
-    var professors = root.child('professors');
-    professors.authWithPassword({
-        "email": $('#user').val(),
-        "password": $('#pass').val()
-    }, function(error, authData) {
-        if (error) {
-            // TODO: error
-            console.log('error signing in');
-        } else {
-            // TODO: success
-            console.log(authData);
-            $.post('localhost:443/login', {'uid': authData.uid, 'type': 'teacher'});
-        }
-    });
+  console.log('hi');
+  var professors = root.child('professors');
+  professors.authWithPassword({
+    "email": $('#user').val(),
+    "password": $('#pass').val()
+  }, function(error, authData) {
+    if (error) {
+      // TODO: error
+      console.log('error signing in');
+    } else {
+      // TODO: success
+      console.log(authData);
+      $.post('localhost:443/login', {
+        'uid': authData.uid,
+        'type': 'teacher'
+      });
+    }
+  });
 });
