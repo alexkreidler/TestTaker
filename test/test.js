833c34655ce17c78d51e5f9893ac84afcde8cc5c
Fox <tomotsmith@gmail.com>
2015-11-23 19:34:44 -0500

    stuff

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 var expect = require('chai').expect
 var mocha = require('mocha')
 var server = require('../server.js')
-mocha.describe('Server', function() {
-  mocha.describe('#toArray()', function() {
+describe('Server', function() {
+  describe('#toArray()', function() {
     expect(toArray({'key': 'value', 'key2': 'value2'})).to.equal(['value', 'value2'])
   });
 });
