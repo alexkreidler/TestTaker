d19999e691c5ec0971f2749e4a58ebf5a11280ec
Fox <tomotsmith@gmail.com>
2015-11-23 19:32:51 -0500

    took .expect away

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 var expect = require('chai').expect
-var mocha = require('mocha').expect
+var mocha = require('mocha')
 var server = require('../server.js')
-describe('Server', function() {
-  describe('#toArray()', function() {
+mocha.describe('Server', function() {
+  mocha.describe('#toArray()', function() {
     expect(toArray({'key': 'value', 'key2': 'value2'})).to.equal(['value', 'value2'])
   });
 });
